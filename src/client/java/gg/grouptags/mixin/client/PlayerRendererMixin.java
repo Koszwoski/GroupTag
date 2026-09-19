b414186a15f23f26d94a27dbb4425445d72a91fc
Koszwoski <fejlkode404@outlook.dk>
2026-09-19 09:38:49 +0200

    Place group tags above Meteor nametags

--- src/client/java/gg/grouptags/mixin/client/PlayerRendererMixin.java
+++ src/client/java/gg/grouptags/mixin/client/PlayerRendererMixin.java
@@ -66,8 +66,11 @@ abstract class PlayerRendererMixin {
         grouptag$submitting = true;
         try {
             state.nameTag = Component.literal(tag.name()).withColor(tag.color() & 0xFFFFFF);
-            state.nameTagAttachment = originalAttachment.add(0.0, -0.28, 0.0);
+            state.nameTagAttachment = originalAttachment.add(0.0, 0.42, 0.0);
+            poses.pushPose();
+            poses.scale(0.82F, 0.82F, 0.82F);
             submitNameTag(state, poses, collector, camera);
+            poses.popPose();
         } finally {
             state.nameTag = originalName;
             state.nameTagAttachment = originalAttachment;
