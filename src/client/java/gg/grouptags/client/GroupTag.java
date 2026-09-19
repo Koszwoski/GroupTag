9c6bd1b641bf08613f79851587eb92c9938fd000
Koszwoski <fejlkode404@outlook.dk>
2026-09-19 09:52:31 +0200

    Include logo metadata in group tags

--- src/client/java/gg/grouptags/client/GroupTag.java
+++ src/client/java/gg/grouptags/client/GroupTag.java
@@ -1,4 +1,6 @@
 package gg.grouptags.client;
 
-public record GroupTag(String name, int color) {
+public record GroupTag(String name, int color, String logoPath, String logoPosition) {
+    public boolean hasLogo() { return logoPath != null && !logoPath.isBlank(); }
+    public boolean logoAfterName() { return "after".equals(logoPosition); }
 }
